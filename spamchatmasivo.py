e1041f9deced5f567c22bf8937aaa5cbf16c7207
Noah002i3 <133623817+Noah002i3@users.noreply.github.com>
2023-06-12 03:14:12 -0500

    Update spamchatmasivo.py

--- spamchatmasivo.py
+++ spamchatmasivo.py
@@ -1,4 +1,4 @@
-import aminofix as amino, concurrent.futures
+import aminofix as amino
 import pyfiglet, colorama
 
 print(colorama.Fore.GREEN)
@@ -8,35 +8,30 @@ print("      [Creditos] [Noah] ")
 print(colorama.Fore.GREEN)
 client=amino.Client()
 client.login(email=input(" - Email: "), password=input(" - Contraseña: "))
-coms=client.sub_clients(size=100)
-for x, name in enumerate(coms.name, 1):
-    print(f"{x}.{name}")
-com_Id = coms.comId[int(input("\nSelecciona la comunidad: ")) - 1]
-sub_client=amino.SubClient(comId=com_Id, profile=client.profile)
-chat=int(input(" - Chats: "))
-message= """[bc]🍑 𝗽.⍺𝗋̼⍺𝖉¡𝗌ɘ ٬٬🔥
+comus=client.sub_clients(size=100)
+for x, name in enumerate(comus.name, 1):
+    print(f" -{x}.{name}")
+comId = comus.comId[int(input("\nSelecciona la comunidad: ")) - 1]
+sub_client=amino.SubClient(comId=comId, profile=client.profile)
+chas=int(input(" - Chats: "))
+mensaje= """texto"""
 
-[Cui] Está comunidad está hecha para ti...~ ven a rolear incluso LEMON!🍋 Que esperas baby?~
-
-[ibc]http://aminoapps.com/invite/WDL30N1FB3"""
-
-titulo = "VEN AMOR 🍑 𝗽.⍺𝗋̼⍺𝖉¡𝗌ɘ ٬٬🔥"
+titulo = "texto"
 
 def chatsxd():
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10000) as executor:
         usersIdxd=sub_client.get_online_users(start=0, size=1000).profile.userId
         userrecent = sub_client.get_all_users(start=0, size=1000, type= "recent").profile.userId
         userrecenteso = sub_client.get_all_users(start=400, size=1000, type= "recent").profile.userId
         userscomplete = [*usersIdxd, *userrecent, *userrecenteso]
         if userscomplete:
             for userId in userscomplete:
-                for i in range(chat):
+                for x in range(chats):
                     try:
-                        sub_client.start_chat(userId=userscomplete, message="")
+                        sub_client.start_chat(userId=userId, message="")
                         chats = sub_client.get_chat_threads(start=0, size=150)
-                        for chat_id , title in zip(chats.chatId,chats.title):
-                            sub_client.edit_chat(chatId=chat_id,title=titulo,viewOnly=200)
-                            sub_client.send_message(chatId=chat_id,message=message)
+                        for chatId , titulo in zip(chats.chatId, chats.title):
+                            sub_client.edit_chat(chatId=chatId,title=titulo,viewOnly=200)
+                            sub_client.send_message(chatId=chatId,message=mensaje)
                     except Exception as e:
                         print(e)
                         pass
