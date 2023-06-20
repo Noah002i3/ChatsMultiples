9c65dfd18c8a03fe25ee11fc160a1ec686504150
Noah002i3 <133623817+Noah002i3@users.noreply.github.com>
2023-06-20 12:00:16 -0500

    Update spamchatmasivo.py

--- spamchatmasivo.py
+++ spamchatmasivo.py
@@ -13,25 +13,25 @@ for x, name in enumerate(comus.name, 1):
     print(f" -{x}.{name}")
 comId = comus.comId[int(input("\nSelecciona la comunidad: ")) - 1]
 sub_client=amino.SubClient(comId=comId, profile=client.profile)
-chas=int(input(" - Chats: "))
+chatsint=int(input(" - Chats: "))
 mensaje= """texto"""
 
-titulo = "texto"
+
 
 def chatsxd():
         usersIdxd=sub_client.get_online_users(start=0, size=1000).profile.userId
         userrecent = sub_client.get_all_users(start=0, size=1000, type= "recent").profile.userId
-        userrecenteso = sub_client.get_all_users(start=400, size=1000, type= "recent").profile.userId
-        userscomplete = [*usersIdxd, *userrecent, *userrecenteso]
-        if userscomplete:
-            for userId in userscomplete:
-                for x in range(chats):
+        userscomplete = [*usersIdxd, *userrecent]
+        if userrecent:
+            for userId in userrecent:
+                for x in range(chatsint):
                     try:
-                        sub_client.start_chat(userId=userId, message="")
+                        sub_client.start_chat(userId=userscomplete, message="")
                         chats = sub_client.get_chat_threads(start=0, size=150)
                         for chatId , titulo in zip(chats.chatId, chats.title):
-                            sub_client.edit_chat(chatId=chatId,title=titulo,viewOnly=200)
+                            sub_client.edit_chat(chatId=chatId,title="texto",viewOnly=200, )
                             sub_client.send_message(chatId=chatId,message=mensaje)
+                            print(" - Finalizado")
                     except Exception as e:
                         print(e)
                         pass
